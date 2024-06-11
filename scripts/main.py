118c292c7b8b2efc1f9edfdfbfbb6d1295f08cb4
Haris Hartanto <muhamadharishartanto@gmail.com>
2024-06-11 15:08:47 +0700

    update: integrate new function in main.py

--- scripts/main.py
+++ scripts/main.py
@@ -13,6 +13,11 @@ from opensearchpy import OpenSearch
 # Load environment variables from .env file
 load_dotenv()
 
+# Neo4j configuration
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USER = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
+
 # OpenSearch configuration
 OPENSEARCH_HOST = os.getenv("OPENSEARCH_HOST")
 OPENSEARCH_PORT = int(os.getenv("OPENSEARCH_PORT"))
@@ -45,12 +50,14 @@ azure_client = AzureOpenAI(
 # Index name for the documents to retrieve from OpenSearch
 document_index = 'produk_hukum_new'
 
+# Topics to be banned from the list
+ban_topics = ['peraturan menteri keuangan',]
 
 def get_doc_topics(doc_title, considerant_content, model=GPT_MODEL, temperature=0.5, top_p=0, max_tokens=500):
     '''
     Get topics from the considerant content.
     '''
-    prompt_path = os.path.join(os.path.dirname(__file__), 'data\\prompt', 'generation.txt')
+    prompt_path = os.path.join(os.path.dirname(__file__), '..\\data\\prompt', 'generation.txt')
     prompt = open(prompt_path, 'r').read().format(doc_title=doc_title, doc_consideration=considerant_content)
 
     response = azure_client.chat.completions.create(
@@ -73,45 +80,55 @@ def get_doc_topics(doc_title, considerant_content, model=GPT_MODEL, temperature=
     return response.choices[0].message.content
 
 
-def topics_extraction(document_id, client=opensearch_client, document_index=document_index):
+def retrieve_doc_info(client, document_id, document_index):
+    try:
+        doc = retrieve_doc(client, document_id, document_index)
+        doc_info = get_doc_info(doc)
+
+    except Exception as e:
+        print(f"Error retrieving document information: {str(e)}", file=sys.stderr)
+        doc, doc_info = None, None
+
+    return doc, doc_info
+
+
+def topics_extraction(doc, doc_info):
     '''
     Extract topics from the document and index the generated topics in OpenSearch.
     '''
-    try:
-        # retrieve document & extract related information and content from the document
-        doc = retrieve_doc(client, document_id, document_index)
-        doc_info = get_doc_info(doc)
+
+    if not doc:
+        print(f"Document with ID '{document_id}' in index '{document_index}' not found.", file=sys.stderr)
+        return None
+        
+    else:
+        doc_title = doc_info['doc_title']
         doc_considerant = get_doc_considerant(doc)
         doc_firstarticle = get_doc_firstarticle(doc)
-        
+            
         # extract keywords from the document json blocks
         doc_headers = get_doc_headers(doc)
         doc_definitionterms = get_doc_definitionterms(doc_firstarticle)
+
+        # preprocess header terms
+        doc_headers = split_headingterms(doc_headers, splitter=" -  ")
 
         # join headers and definition terms list 
         keywords = doc_headers + doc_definitionterms
         keywords = [keyword.lower() for keyword in keywords] # lowercase all keywords.
 
         # extract topics from the document considerant
-        doc_topics = get_doc_topics(doc_info['doc_title'], doc_considerant) # the result is a string and each topic is separated by ";". The last topic has a dot (.) at the end.
+        doc_topics = get_doc_topics(doc_title, doc_considerant) # the result is a string and each topic is separated by ";". The last topic has a dot (.) at the end.
         doc_topics = doc_topics.lower().split(";") # transform doc_topics to list (split by ";") and lowercase all topics.
         doc_topics = [topic.strip() for topic in doc_topics] # remove leading and trailing whitespaces from each topic.
         doc_topics[-1] = doc_topics[-1].replace(".", "") # remove dot (.) from the last element of the topics list.
+        doc_topics = [topic for topic in doc_topics if topic not in ban_topics] # remove ban topics from the list
 
         # result in dictionary/json format
         result = {
-            "Judul": doc_info['doc_title'],
-            "Nomor": doc_info['doc_law_number'],
-            "No": doc_info['doc_number'],
-            "Tahun": doc_info['doc_year'],
-            "Status": doc_info['doc_status'],
             "Kata Kunci": keywords,
             "Topik": doc_topics,
         }
-
-    except Exception as e:
-        print(f"Error extracting topics: {str(e)}", file=sys.stderr)
-        result = None
 
     return result
     
@@ -119,18 +136,25 @@ def topics_extraction(document_id, client=opensearch_client, document_index=docu
 if __name__ == "__main__":
     start_time = time.time()
 
-    output_path = os.path.join(os.path.dirname(__file__), 'data', 'output')
+    output_path = os.path.join(os.path.dirname(__file__), '..\\data', 'output')
     document_id = sys.argv[1]
-    result = topics_extraction(document_id)
 
-    index_generated_topic(document_id, result)
+    document, document_info = retrieve_doc_info(opensearch_client, document_id, document_index)
+    topic_result = topics_extraction(document, document_info)
 
-    if result:
-        json_result = json.dumps(result, indent=4)
+    if topic_result:
+        index_generated_topic(opensearch_client, document_id, topic_result)
+        json_result = json.dumps(topic_result, indent=4)
         with open(f"{output_path}\\{document_id}_topics.json", 'w') as f:
             f.write(json_result)
 
         print(f"Result saved to {output_path}\\{document_id}_topics.json")
+
+        # create neo4j graph
+        with GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD)) as driver:
+            with driver.session() as session:
+                create_graph(session, document_info, topic_result["Topik"])
+                
     else:
         print("Topics extraction failed.")
 
