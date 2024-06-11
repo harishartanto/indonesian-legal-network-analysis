ea3896f88bf87dfdc7b005cab5f17517ed7b4106
Haris Hartanto <muhamadharishartanto@gmail.com>
2024-06-11 09:38:51 +0700

    update: add options for including and excluding specific fields in retrieve_doc function

--- scripts/query_opensearch.py
+++ scripts/query_opensearch.py
@@ -25,25 +25,51 @@ def index_generated_topic(client, document_id, data):
         print(f"Error indexing data: {str(e)}", file=sys.stderr)
 
 
-def retrieve_doc(client, document_id, index_name):
+def retrieve_doc(client, document_id, index_name, included_fields=None, excluded_fields=None):
     """
-    Retrieve documents from an OpenSearch index based on a query with the document ID.
+    Retrieve a document from an OpenSearch index based on its document ID.
+    Optionally, include or exclude specific fields from the document source.
+
+    Parameters:
+    - client: The OpenSearch client instance.
+    - document_id: The ID of the document to retrieve.
+    - index_name: The name of the OpenSearch index.
+    - included_fields: A list of fields to include in the document source (default is None).
+    - excluded_fields: A list of fields to exclude from the document source (default is None).
+
+    Returns:
+    - document: The retrieved document source if found, otherwise None.
     """
+    # Default to empty lists if included_fields or excluded_fields are not provided
+    if included_fields is None:
+        included_fields = []
+    if excluded_fields is None:
+        excluded_fields = []
+
     # Query body to retrieve document based on the document ID (Slug)
     query = {
         "query": {
             "match": {
                 "Slug": document_id
             }
+        },
+        "_source": {
+            "includes": included_fields,
+            "excludes": excluded_fields
         }
     }
 
     try:
+        # Execute the search query
         response = client.search(index=index_name, body=query)
-        document = response['hits']['hits'][0]['_source']
+        # Check if any documents were found
+        if response['hits']['total']['value'] > 0:
+            document = response['hits']['hits'][0]['_source']
+        else:
+            document = None
 
     except Exception as e:
-        print(f"Error retrieving documents: {str(e)}")
         document = None
+        print(f"Error retrieving document: {str(e)}", file=sys.stderr)
 
     return document
